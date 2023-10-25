d14b241dbe3b72176e92d54e9513445c2c187e75
KaganGunturk <yasirkagang@gmail.com>
2023-10-25 22:04:33 +0300

    25.10.2023

---
+++ SpringBoot/02-spring-boot-spring-core/01-constructer-injection/src/main/java/com/kagan/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.kagan.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ SpringBoot/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/kagan/springcoredemo/rest/DemoController.java
@@ -0,0 +1,22 @@
+package com.kagan.springcoredemo.rest;
+
+import com.kagan.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
